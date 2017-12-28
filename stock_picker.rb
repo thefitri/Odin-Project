21427124c0a4dd4c176258b18b2cc86beb842ee2
Fitri Ali <mail@fitri.co>
2017-12-28 17:20:04 +0800

    Update stock_picker.rb

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,1 +1,14 @@
+def stock_picker price_array
+  matrix = {}
+  last_day = price_array.length - 1
+  (0..last_day).each do |day_a|
+    (day_a..last_day).each do |day_b|
+      matrix[[day_a,day_b]] = price_array[day_b]- price_array[day_a]
+    end
+  end
+  max_return = matrix.values.max
+  max_return_days = matrix.select{ |days,value| value == max_return}
+  return max_return_days.first[0]
+end
 
+puts "Usage:\nstock_picker([stock price array])"
