7cf234d603430216ec1508a7292554facb541e5a
Fitri Ali <mail@fitri.co>
2017-12-29 09:46:20 +0800

    Update substrings.rb

--- substrings.rb
+++ substrings.rb
@@ -4,9 +4,7 @@ def substrings str, dictionary
   dictionary.map {|dictionary_word| match[dictionary_word.strip.downcase] = 0}
   
   words.map do |word|
-    match.map do |key, value|
-      match[key] += word =~ /#{key}/ ? 1 : 0
-    end
+    match.map {|key, value| match[key] += word =~ /#{key}/ ? 1 : 0}
   end
   
   return match.select {|key, value| value > 0}
@@ -14,5 +12,4 @@ end
 
 word = "Howdy partner, sit down! How's it going?"
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-
 puts "usage:\tsubstrings([word:string], [dictionary:array])"
